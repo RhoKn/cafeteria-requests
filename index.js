47d1c286a1c463b2bcb9d53b790d7b465764edc7
Ruben Gonzalez <rubengonzalez@MBP-de-Ruben.example.org>
2019-03-01 10:49:55 -0700

    Cambio en los mensajes de inicio

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/cafeteria',{ useNewUrlParser: true }).then(()=>{
-    console.log('La conexion seasdas hizo chidamente');
+    console.log('Iniciando conexión con base de datos');
     app.listen(port,()=>{
         console.log('Conexión realizada');
     });
