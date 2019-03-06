452f8b00d3cb6d1a382a7fc0bfaedfc43da090ed
Ruben Gonzalez <rubengonzalez@MBP-de-Ruben.azmodan.gammapartners.com>
2019-03-06 14:38:01 -0700

    Se terminan las rutas y el funcionamiento del CRUDproductos, se realizaron ajustes a las rutas de providers y se cambio el gitignore

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -3,7 +3,7 @@ const providersController = require('../controllers/providersController');
 var api = express.Router();
 
 api.get('/all/:page?/:order?',providersController.listAll);
-api.get('/view/:id',providersController.viewUser);
+api.get('/view/:id',providersController.viewProvider);
 api.post('/create',providersController.createProvider);
 api.put('/update/:id',providersController.updateProvider);
 api.delete('/delete/:id',providersController.deleteProvider);
