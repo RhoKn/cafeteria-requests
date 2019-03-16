65c84480f1927aff43c05959487db3f438453706
Andres Hernandez Zamora <a301540@uach.mx>
2019-03-15 22:50:49 -0700

    Se cambia la paginacion

--- controllers/requestsController.js
+++ controllers/requestsController.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 
 function viewAll (req,res){
     let page = req.params.page ? req.params.page : 1;
-    const request_per_page =10;
+    const request_per_page =25;
     const order = req.params.order ? req.params.order : 'created_at';
     Request.find().sort(order).populate('dRoom').paginate(page, request_per_page, (err, requests, total)=>{
         if(err) return res.status(500).send({message: 'Hubo un error en la petición'});
